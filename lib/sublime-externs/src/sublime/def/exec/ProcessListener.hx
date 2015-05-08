7323018c6d16d90b02fea4d782517223dfbded48
frabbit <frabbit77@googlemail.com>
2015-05-08 19:05:56 +0200

    fix import of ProcessListener

--- lib/sublime-externs/src/sublime/def/exec/ProcessListener.hx
+++ lib/sublime-externs/src/sublime/def/exec/ProcessListener.hx
@@ -6,12 +6,15 @@ import python.Bytes;
 import sublime.def.exec.AsyncProcess;
 
 
-
-
-@:pythonImport("Default.exec", "ProcessListener")
 @:keep extern interface ProcessListener
 {
     public function on_data(proc:AsyncProcess, data:Bytes):Void;
 
     public function on_finished(proc:AsyncProcess):Void;
+
+    public static function __init__ ():Void {
+		python.Syntax.pythonCode("import Default");
+		python.Syntax.pythonCode("stexec = getattr(Default, 'exec')");
+		python.Syntax.pythonCode("sublime_def_exec_ProcessListener = stexec.ProcessListener");
+	}
 }
